d66743bcf5103714876ce827893c48bc4c4591bf
Qiumix <j487428862@gmail.com>
2025-03-19 19:42:33 +0800

    添加打印三角形

--- py/class/class5/test.py
+++ py/class/class5/test.py
@@ -1,3 +1,19 @@
-for i in range(1, 10):
-    for j in range(1, i + 1):
-        print(f"{j} * {i} = {i * j}", end = ("\n" if i == j else "\t"))
+# for i in range(1, 10):
+#     for j in range(1, i + 1):
+#         print(f"{j} * {i} = {i * j}", end = ("\n" if i == j else "\t"))
+
+n = 7
+# for i in range(n):
+#     for j in range(n - i):
+#         print("  ", end = '')
+#     for i in range(2 * i + 1):
+#         print("* ", end = '')
+#     print("")
+
+# for i in range(n):
+#     for j in range(n - i + 1):
+#         print("  ", end = '')
+#     for j in range(i):
+#         print(j + 1, end = ' ')
+#     print("")
+
