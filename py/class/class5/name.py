cd21d4ca1d7d889190d02d3ce5c36a1cd8fef404
Qiumix <j487428862@gmail.com>
2025-03-19 20:06:53 +0800

    a

--- py/class/class5/name.py
+++ py/class/class5/name.py
@@ -1,1 +1,7 @@
 quit = ":q!"
+while 1:
+    s = input("tap in a name(is the name is \":q!\", then quit)")
+    if s == quit:
+        print("exit the program")
+        break
+    print(f"the name that you tap in is {s}")
