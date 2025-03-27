46113053db2b0abcecc17f8e392722b7ed99bae7
Qiumix <j487428862@gmail.com>
2025-03-27 21:03:23 +0800

    add chapter 1.6.py for my high order function learning.

--- py/sicp/chapter1.6.py
+++ py/sicp/chapter1.6.py
@@ -33,14 +33,11 @@ def improve(update, close, guess = 1):
         guess = update(guess)
     return guess
 
-
 def golden_update(guess):
     return 1 / guess + 1
 
-
 def approx_eq(x, y, tolerance=1e-20):
     return abs(x - y) < tolerance
-
 
 def square_close_to_successor(guess):
     return approx_eq(guess * guess, guess + 1)
