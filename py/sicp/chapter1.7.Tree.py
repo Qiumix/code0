167361c33b2d71bb926d6f21379c16c76045ed6d
Qiumix <j487428862@gmail.com>
2025-03-30 18:41:13 +0800

    change

--- py/sicp/chapter1.7.Tree.py
+++ py/sicp/chapter1.7.Tree.py
@@ -12,18 +12,20 @@ def f_then_g(f, g, n):
 
 grow = lambda n: f_then_g(grow, print, n // 10)
 shrink = lambda n: f_then_g(print, shrink, n // 10)
-import trace
+from ucb import trace
+@trace
 def fib(x):
-    if x == 1:
-        return 1
-    elif x == 0:
+    if x == 1 or x == 0:
+        return x
+    elif x < 0:
+        return fib(x + 2) - fib(x + 1)
+    return fib(x - 1) + fib(x - 2)
+@trace
+def sp(n, m):
+    if n < 0:
         return 0
-    else:
-        return fib(x + 1) + fib(x + 2)
-fib(4)
-
-# 这是master分支的更改
-
-# 更改
-
-# 这是一个分歧点
+    if n == 0 or m == 1:
+        return 1
+    return sp(n - m, m) + sp(n, m - 1)
+t = sp(6, 4)
+print("\n\n", t)
